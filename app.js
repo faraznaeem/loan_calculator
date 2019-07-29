f1b5e2217af51f08cc969a33b17f5f8b6cfd4edc
faraznaeem <faraznaeem@gmail.com>
2019-07-29 12:33:21 +0200

    Adds calculation variables

--- app.js
+++ app.js
@@ -6,10 +6,14 @@ function calculateResults(e) {
     const amount = document.getElementById('amount');
     const interest = document.getElementById('interest');
     const years = document.getElementById('years');
-    const monthlyPayments = document.getElementById(monthly-payment);
-    const totalPayments = document.getElementById(total-payment);
-    const totalInterest = document.getElementById(total-interest);
+    const monthlyPayments = document.getElementById('monthly-payment');
+    const totalPayments = document.getElementById('total-payment');
+    const totalInterest = document.getElementById('total-interest');
 
+    const principal = parseFloat(mount.value);
+    const calculationInterest = parseFloat(interest.value) / 100 / 12;
+    const calculatePayments = parseFloat(years.value) * 12;
+    
     e.preventDefault();
 
 }
